8e6069e4e413fd1434625b4ade68827e44c48aeb
WaldperlachFabi <80318133+WaldperlachFabi@users.noreply.github.com>
2023-04-20 19:32:31 +0200

    Add files via upload

---
+++ src/main/java/de/waldperlachfabi/app/MainActivity.java
@@ -0,0 +1,36 @@
+package de.waldperlachfabi.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.example.app.R;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.loadUrl("https://waldperlachfabi.de");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
